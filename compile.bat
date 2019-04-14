3e2c682ea2f5fde83aaba4d8ce68a13aaace0012
Ttatanepvp123 <ttatane974@gmail.com>
2019-04-14 19:27:23 +0200

    repo cleaning

--- compile.bat
+++
@@ -1,2 +0,0 @@
-gcc *.c -o bin/Prog -I include -L lib -lSDL2_image.dll -lmingw32 -lSDL2main -lSDL2.dll -lSDL2_image.dll -lSDL2_image -lSDL2_mixer.dll -lSDL2_mixer -lSDL2_ttf.dll -lSDL2_ttf -lws2_32
-pause
--- server/compile_-_server.bat
+++
@@ -1,2 +0,0 @@
-gcc *.c -o server -I include -L lib -lws2_32
-pause
